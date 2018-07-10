ec97ba813220de96283b31035ebc5f20364286a4
David Meir-Levy <davidmeirlevy@gmail.com>
2018-07-10 12:12:10 +0300

    minor fixes to router types

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import {sharedState} from './store';
 import './sass/main.scss';
-import {Register} from './modules/pre-game/register/register';
 import { createRouter } from './router';
 
 // tslint:disable-next-line:no-unused-expression
