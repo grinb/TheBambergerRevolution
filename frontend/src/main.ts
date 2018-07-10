83e0816d1bf1bf800292ac6cd3251e9956c4ccf2
lironh <lironenk@gmail.com>
2018-07-10 12:00:30 +0300

    feat(added router)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,13 @@ import Vue from 'vue';
 import {sharedState} from './store';
 import './sass/main.scss';
 import {Register} from './modules/pre-game/register/register';
+import { createRouter } from './router';
 
 // tslint:disable-next-line:no-unused-expression
 new Vue({
     el: '#app-main',
+    router: createRouter(),
     data: {
         sharedState
-    },
-    components: {
-        Register
     }
 });
